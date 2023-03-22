9a4bac912ffc7aed650d70dbd7bbf7cfc17e1fd8
Nitu Alexandru <nitualecsandru@gmail.com>
2023-03-23 00:29:31 +0200

    adding images and index.html - index.js modifications

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-console.log("hello world");
-console.log("hello world");
-console.log("hello world");
-console.log("hello world");
+var DEFAULT_RATE = 0.10;
+var rate = 0.05;
+
+function getRate(){
+    if(rate > 0.01){
+        let rate = DEFAULT_RATE;
+        console.log(rate);
+        //return rate;
+    }
+    return rate;
+}
+
+console.log("Your rate is ", getRate());
